3c0d8db23954463be4f8e810920184e2e08b7df2
Emmanuel Nyarko <joe@Emmanuels-MacBook-Pro.local>
2024-05-24 14:03:07 +0000

    check D mangling on windows

--- stat.d
+++ stat.d
@@ -1,11 +1,15 @@
 extern(C++, class):
 struct Map(T, U)
 {
+    this(int i);
     Map* make();
 }
 
 
 void main()
 {
-    auto mymap = Map!(int, char).make();
+    auto mymap = Map!(int, char)(5);
+    mymap.make();
+
+    //auto mymap = Map!(int, char).make();
 }
